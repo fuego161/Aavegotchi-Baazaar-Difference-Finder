2054313d714ccf8d09239c60523c8be1ad8b0757
fuego161 <46138241+fuego161@users.noreply.github.com>
2021-03-13 14:55:11 +0000

    Removed unused constructor

--- src/components/Views.js
+++ src/components/Views.js
@@ -3,10 +3,6 @@ import { PriceDifferences } from './PriceDifferences';
 import { Filtering } from './Filtering';
 
 export class Views extends Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		if (this.props.view === 'price-differences') {
 			return <PriceDifferences view={this.props.view} />
