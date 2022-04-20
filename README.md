f1967507297f2d92c4f4512ab054d09c03910a65
fuego161 <46138241+fuego161@users.noreply.github.com>
2022-04-20 12:21:23 +0100

    Update README and intro

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Aavegotchi Baazaar Difference Finder
 
-First project with React. Not yet a finished product
+**Note:** Aavegotchi has continued development since this project was created. The initial use case for this project has passed. Whilst features still work, some things may not behave as intended. This site is now purely a work showcase.
+
+An app built to help users find deals within the Aavegotchi Baazaar.
+
+At the time of development, the Aavegotchi Baazaar was extremely limited. Only a certain amount of listings could be shown at one time, this would push older listings from being visible. This app was made so people could find listings which were no longer visible, either to better price their own items before creating listings, or to find deals.
+
+There are two tools within the app. These are the "Price Difference Finder" and "Baazaar Filtering".
+
+## Price Difference Finder
+
+The idea of this section is to find the cheapest two listings for an item. Once found, if the difference is above your selected "Percentage" the app will show a table containing the five cheapest items.
+
+This helps you find the individual floors of items, as well as if there's one sneakily cheaper than the rest.
+
+The price difference finder lets you select between either Wearables or Consumables. It also lets you filter by rarity.
+
+## Baazaar Filtering
+
+This section allows you to choose between Closed Portals and Open Portals and filter the visibility and order.
+
+Visibility allows you to choose to show "All the listings", "Listings only showing on the Baazaar" or "Listings excluding those on the Baazaar". From here you can then select which order to show the listings, these orders are "Recent", "Ascending" price, or "Descending".
+
+A good use case for this is checking to see for any Closed Portals from only listings which fell from the Baazaar, ordered by cheapest first. You can then see if there are deals to be snapped up and click on a portal to open the Baazaar page on the Aavegotchi site.
--- src/components/layout/Intro.js
+++ src/components/layout/Intro.js
@@ -8,18 +8,20 @@ export class Intro extends Component {
 				<div className="intro">
 					<p className="intro__text">This app is built to help you find deals within the Aavegotchi Baazaar. There are two tools within this app, toggle between these with the two buttons above. See an overview of them below.</p>
 
+					<p className="intro__text">Note: Aavegotchi has continued development since this project was created. The initial use case for this project has passed. Whilst features still work, some things may not behave as intended. This site is now purely a work showcase.</p>
+
 					<div className="intro__box">
 						<h2>Price Difference Finder</h2>
-						<p>The idea of this section is to find the cheapest two listings of an item. Once found, if the difference is above your selected "Percentage" the app will show a table containing the five cheapest items.</p>
+						<p>The idea of this section is to find the cheapest two listings for an item. Once found, if the difference is above your selected "Percentage" the app will show a table containing the five cheapest items.</p>
 						<p>This helps you find the individual floors of items, as well as if there's one sneakily cheaper than the rest.</p>
-						<p>The price difference finder let's you select between either Wearables or Consumables. It also lets you filter by rarity.</p>
+						<p>The price difference finder lets you select between either Wearables or Consumables. It also lets you filter by rarity.</p>
 					</div>
 
 					<div className="intro__box">
 						<h2>Baazaar Filtering</h2>
-						<p>This section allows you to choose between Closed Portals and Open Portals (Gothci's too once I sort out some issues), and filter the visibility and order.</p>
-						<p>Visibility allows you choose to show "All the listings", "Listings only showing on the Baazaar" or "Listings excluding those on the Baazaar". From here you can then select which order to show the listings, these orders are "Recent", "Ascending" price, or "Descending".</p>
-						<p>A good use case for this is checking to see for any Closed Portals from only listings which fell from the Baazaar, ordered by cheapest first. You can then see if there's and deals to be snapped up and click on a portal to open the Baazaar page on the Aavegotchi site.</p>
+						<p>This section allows you to choose between Closed Portals and Open Portals and filter the visibility and order.</p>
+						<p>Visibility allows you to choose to show "All the listings", "Listings only showing on the Baazaar" or "Listings excluding those on the Baazaar". From here you can then select which order to show the listings, these orders are "Recent", "Ascending" price, or "Descending".</p>
+						<p>A good use case for this is checking to see for any Closed Portals from only listings which fell from the Baazaar, ordered by cheapest first. You can then see if there are deals to be snapped up and click on a portal to open the Baazaar page on the Aavegotchi site.</p>
 					</div>
 				</div>
 			</>
